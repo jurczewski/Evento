60786b4307d7b2d7fc7aa72fb390f1ddcc04cb3f
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2019-03-23 18:18:42 +0100

    2. Repositories done

---
+++ Evento.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Evento.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Evento.Core.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetAsync(Guid id);
+        Task<User> GetAsync(string email);
+        Task<IEnumerable<User>> BrowseAsync(string name = "");
+        Task AddAsync(User user);
+        Task UpdateAsync(User user);
+        Task DeleteAsync(User user);
+    }
+}
