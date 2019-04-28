9307c32800d9e3f8dab312487512db3b5c13c2bd
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2019-04-28 20:28:41 +0200

    [ADDED] JWTHandlers and DTO

---
+++ Evento.Infrastructure/Services/IJwtHandler.cs
@@ -0,0 +1,10 @@
+﻿using Evento.Infrastructure.DTO;
+using System;
+
+namespace Evento.Infrastructure.Services
+{
+    public interface IJwtHandler
+    {
+        JwtDto CreateToken(Guid userId, string role);
+    }
+}
