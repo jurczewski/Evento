16a5d2f05b9b150a19009eeb113a3bbfcdaec144
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2019-05-08 19:52:15 +0200

    [ADDED] Test projects

---
+++ Evento.Tests/Respositories/UserRepositoryTests.cs
@@ -0,0 +1,26 @@
+﻿using Evento.Core.Domain;
+using Evento.Core.Repositories;
+using Evento.Infrastructure.Repositories;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Evento.Tests.Respositories
+{
+    public class UserRepositoryTests
+    {
+        [Fact]
+        public async Task when_adding_new_user_it_should_be_added_correcty_to_the_list()
+        {
+            //Arrange
+            var user = new User(Guid.NewGuid(), "user", "test", "test@test.com", "secret");
+            IUserRepository repository = new UserRepository();
+            //Act
+            await repository.AddAsync(user);
+
+            //Assert
+            var existingUser = await repository.GetAsync(user.Id);
+            Assert.Equal(user, existingUser);
+        }
+    }
+}
