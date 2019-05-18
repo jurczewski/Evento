0528d1b292d769b5949a50ad1068ebfaca93dda4
Bartosz Jurczewski <bartek.jurczewski@gmail.com>
2019-05-18 13:13:04 +0200

    Integration test fixed (references)

--- Evento.Tests.EndToEnd/Controllers/EventsControllerTests.cs
+++ Evento.Tests.EndToEnd/Controllers/EventsControllerTests.cs
@@ -18,40 +18,21 @@ namespace Evento.Tests.EndToEnd.Controllers
             _factory = factory;
         }
 
-        //[Fact]
-        ////[InlineData("/events")]
-        //public async Task Fetching_events_should_return_not_empty_collection()
-        //{
-        //    // Arrange
-        //    var client = _factory.CreateClient();
+        [Fact]
+        //[InlineData("/events")]
+        public async Task Fetching_events_should_return_not_empty_collection()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
 
-        //    // Act
-        //    var response = await client.GetAsync("events");
-        //    var content = await response.Content.ReadAsStringAsync();
-        //    var events = JsonConvert.DeserializeObject<IEnumerable<EventDTO>>(content);
+            // Act
+            var response = await client.GetAsync("events");
+            var content = await response.Content.ReadAsStringAsync();
+            var events = JsonConvert.DeserializeObject<IEnumerable<EventDTO>>(content);
 
-        //    // Assert
-        //    response.StatusCode.Should().BeEquivalentTo(HttpStatusCode.OK);
-        //    events.Should().NotBeEmpty();
-        //}
-        //[Theory]
-        //[InlineData("/")]
-        //[InlineData("/Index")]
-        //[InlineData("/About")]
-        //[InlineData("/Privacy")]
-        //[InlineData("/Contact")]
-        //public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
-        //{
-        //    // Arrange
-        //    var client = _factory.CreateClient();
-
-        //    // Act
-        //    var response = await client.GetAsync(url);
-
-        //    // Assert
-        //    response.EnsureSuccessStatusCode(); // Status Code 200-299
-        //    Assert.Equal("text/html; charset=utf-8",
-        //        response.Content.Headers.ContentType.ToString());
-        //}
+            // Assert
+            response.StatusCode.Should().BeEquivalentTo(HttpStatusCode.OK);
+            events.Should().NotBeEmpty();
+        }       
     }
 }
